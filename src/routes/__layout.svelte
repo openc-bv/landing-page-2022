81e1ead0dd09d1beaf5a4adaac0cb1368a5aa7fa
Romein van Buren <romein@vburen.nl>
2022-03-10 18:31:26 +0100

    Footer conform design system
    Signed-off-by: Romein van Buren <romein@vburen.nl>

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,8 +3,9 @@
 	import '$lib/global.scss';
 	import { page } from '$app/stores';
 	import NavigationBar from '@openc-bv/design-system/components/navigation-bar.svelte';
-	import Footer from '$lib/components/footer.svelte';
+	import Footer from '@openc-bv/design-system/components/footer.svelte';
 	import { navigationItems } from '$lib/constants';
+import IconLinkedin from '$lib/gfx/icon-linkedin.svelte';
 </script>
 
 <NavigationBar path={$page.url.pathname} items={navigationItems} />
@@ -13,4 +14,11 @@
 	<slot />
 </main>
 
-<Footer />
+<Footer>
+	<div slot="social">
+		<a href="https://www.linkedin.com/company/openc-net/" target="_blank">
+			<IconLinkedin />
+			<span class="sr">LinkedIn</span>
+		</a>
+	</div>
+</Footer>
